5069b100cc7144270f85d4704e4f8554c9348234
Taccio Bernal <ttatiuss@MacBook-Air-de-Taccio.local>
2026-04-30 11:58:02 -0300

    html,css e js

---
+++ script.js
@@ -0,0 +1,14 @@
+let map = L.map("map").setView([-19.922731, -43.945094], 13);
+
+L.tileLayer(
+  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
+  {
+    attribution:
+      "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012",
+    maxZoom: 19,
+  },
+).addTo(map);
+
+window.addEventListener("load", () => {
+  map.invalidateSize();
+});
